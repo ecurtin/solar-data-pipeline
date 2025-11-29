f2bcef5b354a600b9e53500dc37a29d63d61729f
Ryan Curtin <ryan@ratml.org>
2025-11-29 14:04:36 +0000

    Set working directory for cron.

--- predict.sh
+++ predict.sh
@@ -2,5 +2,6 @@
 #
 # Simple test script to test the pipeline each day in a cron job.
 
+cd /home/ryan/src/solar-data-pipeline/
 python3 pull_cloud_cover.py
 ./battery_target_setter/battery_target_setter | mail -s "Solar forecast" ryan@ratml.org emily@framebit.org
