e967833acaa3b0adfaa581e276b2b78364e63235
Ryan Curtin <ryan@ratml.org>
2026-02-14 23:19:21 +0000

    Finish data ingest from influx and do very basic modeling.

--- battery_target_setter/battery_target_setter.cpp
+++ battery_target_setter/battery_target_setter.cpp
@@ -14,31 +14,40 @@ void onPublish(struct mosquitto *mosq, void *obj, int mid);
 static bool mqttConnected;
 static bool mqttPublished;
 
-fmat influxToArma(const std::vector<Point>& points)
+// join algorithm:
+//  - sort all input tables by time value (should already be sorted...)
+//  - make sure all input tables have the same size
+//  - make sure all time columns match in each input table
+//  - paste them together with arma::join_rows() after dropping time index
+
+void influxToArma(const vector<Point>& points, uvec& timestamps, fmat& output)
 {
-  fmat m(points[0].getFieldSet().size(), points.size());
+  output.set_size(points[0].getFieldSet().size(), points.size());
+  timestamps.set_size(points.size());
 
   for (size_t i = 0; i < points.size(); ++i)
   {
+    // Extract the timestamp into the relevant element of the timestamp vector.
+    timestamps[i] = chrono::duration_cast<chrono::seconds>(
+        points[i].getTimestamp().time_since_epoch()).count();
+
     // Assumptions: fields will always be in the same order;
     // all fields are ints, floats, or doubles.
     for (size_t j = 0; j < points[i].getFieldSet().size(); ++j)
     {
       const Point::FieldValue& f = points[i].getFieldSet()[j].second;
       if (f.index() == 0)
-        m(j, i) = (float) get<int>(f);
+        output(j, i) = (float) get<int>(f);
       else if (f.index() == 1)
-        m(j, i) = (float) get<long long int>(f);
+        output(j, i) = (float) get<long long int>(f);
       else if (f.index() == 3)
-        m(j, i) = (float) get<double>(f);
+        output(j, i) = (float) get<double>(f);
       else if (f.index() == 5)
-        m(j, i) = (float) get<unsigned int>(f);
+        output(j, i) = (float) get<unsigned int>(f);
       else if (f.index() == 6)
-        m(j, i) = (float) get<unsigned long long int>(f);
+        output(j, i) = (float) get<unsigned long long int>(f);
     }
   }
-
-  return m;
 }
 
 int main()
@@ -49,6 +58,9 @@ int main()
     cerr << "Error initializing mosquitto!  Sorry." << endl;
     exit(1);
   }
+
+  fmat dailyForecast, forecastHistory, generationHistory;
+  uvec dailyForecastTimes, forecastHistoryTimes, generationHistoryTimes;
 
   unique_ptr<InfluxDB> influxdb = InfluxDBFactory::Get(
       "http://localhost:8086?db=weather");
@@ -61,7 +73,7 @@ int main()
     oss << "Expected 24 rows in daily forecast; got " << points.size() << "!";
     throw runtime_error(oss.str());
   }
-  fmat dailyForecast = influxToArma(points);
+  influxToArma(points, dailyForecastTimes, dailyForecast);
 
   // Now get all of our historical data.
   points = influxdb->query("SELECT * FROM forecast_history");
@@ -70,18 +82,79 @@ int main()
     throw runtime_error("Received empty results from forecast_history "
         "measurement!");
   }
-  fmat forecastHistory = influxToArma(points);
+  influxToArma(points, forecastHistoryTimes, forecastHistory);
 
   cout << "Daily forecast size:   " << dailyForecast.n_rows << " x "
       << dailyForecast.n_cols << "." << endl;
   cout << "Forecast history size: " << forecastHistory.n_rows << " x "
       << forecastHistory.n_cols << "." << endl;
 
+  // Now pull historical data for the total time range
+
+  // SELECT mean(combined) from "PV power hourly" group by time(1h) where
+  //  time >= minTime and time <= maxTime
+  unique_ptr<InfluxDB> influxdbSolar = InfluxDBFactory::Get(
+      "http://localhost:8086?db=solar_assistant");
+  points = influxdbSolar->query(
+      "SELECT mean(combined) from \"PV power hourly\" GROUP BY time(1h);");
+  if (points.size() == 0)
+  {
+    throw runtime_error("Received empty results from \"PV power hourly\" "
+        "measurement!");
+  }
+  influxToArma(points, generationHistoryTimes, generationHistory);
+
+  cout << "Generation history size: " << generationHistory.n_rows << " x "
+      << generationHistory.n_cols << "." << endl;
+
+  // Find the time indices in generationHistoryTimes that match time indices in
+  // forecastHistoryTimes.
+  uvec innerGenTimes, innerForecastTimes, commonTimes;
+  intersect(commonTimes, innerGenTimes, innerForecastTimes,
+      generationHistoryTimes, forecastHistoryTimes);
+
+  cout << "Times in common: " << commonTimes.n_elem << "." << endl;
+
+  // Filter the generation and forecast histories down to only those hours they
+  // share in common.
+  fmat innerForecast = forecastHistory.cols(innerForecastTimes);
+  frowvec innerGen = generationHistory.cols(innerGenTimes);
+
+  // Train a Bayesian linear regression model on random data and make predictions.
+  // All data and responses are uniform random; this uses 10 dimensional data.
+  // Replace with a Load() call or similar for a real application. 
+  //arma::mat dataset(10, 1000, arma::fill::randu); // 1000 points. 
+  //arma::rowvec responses = arma::randn<arma::rowvec>(1000); 
+  //arma::mat testDataset(10, 500,arma::fill::randu); // 500 test points. 
+  
+  mlpack::DecisionTreeRegressor blr; // Step 1: create model.
+  
+  blr.Train(innerForecast, innerGen); // Step 2: train model.
+  
+  arma::frowvec predictions;
+  
+  blr.Predict(dailyForecast, predictions); // Step 3: use model to predict. 
+  
+  // Print some information about the test predictions. 
+  std::cout << arma::accu(predictions > 0.6) << " test points predicted to have"    
+  << " responses greater than 0.6." << std::endl;
+  std::cout << arma::accu(predictions < 0) << " test points predicted to have "
+  << "negative responses." << std::endl;
+
+  // Print predictions
+  std::cout << "Predictions: " << predictions / 1000 << std::endl;
+
+  // Now compute the RMSE on the training set.
+  blr.Predict(innerForecast, predictions);
+  cout << "RMSE on the training set: "
+      << sqrt(mean(pow(predictions - innerGen, 2))) << "." << std::endl;
+
   exit(0);
 
   // TODO: retrain model?
   //  - pull historical data
   //  - pull generation data (kWh / hr)
+  //  - join all the data together
   //  - fit it with a regressor
   //  - print R^2 score
   //  - serialize the model ????? for debugging
