bd42a678a4ddcf38293e08fe7a4530cdbab7b869
Ryan Curtin <ryan@ratml.org>
2026-04-22 02:06:07 +0000

    Add script to compute sun alt/az position.

---
+++ compute_sun_positions.py
@@ -0,0 +1,45 @@
+from astropy.coordinates import SkyCoord, AltAz, EarthLocation, get_sun
+from astropy.time import Time
+from astropy import units as u
+import pandas as pd
+import numpy as np
+import influxdb_client
+from influxdb_client.client.write_api import SYNCHRONOUS
+
+LATITUDE = 33.78317
+LONGITUDE = -84.40153
+
+def compute_sun_altaz(t, lat, lon):
+  """
+  Compute the altitude and azimuth of the sun at the given Earth coordinates and
+  the given time.
+  """
+  s = get_sun(Time(t))
+  e = EarthLocation(lat=(lat * u.deg), lon=(lon * u.deg))
+  altaz = s.transform_to(AltAz(obstime=t, location=e))
+  return (altaz.alt.degree, altaz.az.degree)
+
+
+df = pd.DataFrame({ 'time': pd.date_range(start='2025-08-01',
+                                          end='2050-01-01',
+                                          freq='h',
+                                          tz='America/New_York') })
+
+print(df)
+
+alt_azs = [compute_sun_altaz(t, LATITUDE, LONGITUDE) for t in df['time']]
+alts = [x[0] for x in alt_azs]
+azs = [x[1] for x in alt_azs]
+
+df['sun_alt'] = alts
+df['sun_az'] = azs
+
+df = df.set_index('time')
+
+# Now export the data to Influx.
+client = influxdb_client.InfluxDBClient(url="http://localhost:8086")
+write_client = client.write_api(write_options=SYNCHRONOUS)
+write_client.write("weather",
+                   "my-org",
+                   record=df,
+                   data_frame_measurement_name="sun_altaz")
