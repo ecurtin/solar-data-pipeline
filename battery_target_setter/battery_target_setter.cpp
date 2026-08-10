1cbf044297439ec193c679cb65cf84815a013a25
Ryan Curtin <ryan@ratml.org>
2026-08-10 00:46:17 +0000

    Shoot for 85% instead.

--- battery_target_setter/battery_target_setter.cpp
+++ battery_target_setter/battery_target_setter.cpp
@@ -330,15 +330,15 @@ int main()
   const double maxSurplus = surplus.max();
   const double minBasedBatteryTarget = (minSurplus > 0) ? 0.2 :
       std::min(1.0, 0.2 + -minSurplus / 28800);
-  // If the maximum expected battery level during the day is less than 90%,
+  // If the maximum expected battery level during the day is less than 85%,
   // boost up what we are starting at.
-  const double batteryTarget = (maxSurplus < (0.9 * 28800)) ?
-      ((0.9 * 28800 - maxSurplus) / 28800) : minBasedBatteryTarget;
+  const double batteryTarget = (maxSurplus < (0.85 * 28800)) ?
+      ((0.85 * 28800 - maxSurplus) / 28800) : minBasedBatteryTarget;
   cout << "Battery level at 7am should be set to " << setprecision(2)
       << (100.0 * batteryTarget) << "%." << endl;
   if (batteryTarget != minBasedBatteryTarget)
   {
-    cout << "(Battery level adjusted to keep maximum level at least 90%.)"
+    cout << "(Battery level adjusted to keep maximum level at least 85%.)"
         << endl;
   }
 
